262cdda5277be4dd8aa78303aaf29c3c5a7073d0
niloocampos <nvancity@gmail.com>
2024-02-26 20:28:32 -0800

    Created a reusable hero component

---
+++ src/components/hero.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export default function Hero({ imgName, imgAlt, title }) {
+  const imgSrc = `/${imgName}.jpg`;
+
+  return (
+  <div className="relative h-screen">
+    <div className="absolute -z-10 inset-0">
+      <Image src={imgSrc} alt={imgAlt} fill style={{objectFit: 'cover'}}/>
+      <div className="absolute inset-0 bg-gradient-to-r from-slate-900"></div>
+    </div>
+    <div className="pt-48 flex justify-center items-center">
+      <h1 className="text-white text-6xl">{title}</h1>
+    </div>
+  </div>
+  );
+};
